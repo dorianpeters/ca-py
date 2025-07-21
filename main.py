48333fe66515d01903e50bb880e4f1cf1a4b15e9
Dorian Peters <dpeters08@gmail.com>
2025-07-21 01:33:37 -0700

    Implement date calculation functionality and add initial package configuration

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from datetime import datetime, timedelta, date
+
 def main():
-    print("Hello from ca-py!")
+    startDateStr = input("Enter the starting date in MM-DD-YYYY format - default = today: ")
+
+    dateObject = datetime.strptime(startDateStr, '%m-%d-%Y').date() if startDateStr else date.today()
+
+    addDays = input("Enter the number of days to add: ")
+    newDate = dateObject + timedelta(days=int(addDays))
+
+    print('The new date is:')
+    print(newDate)
+
 
 
 if __name__ == "__main__":
